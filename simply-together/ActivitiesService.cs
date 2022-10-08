7dbfa97ce6b7d0d71de658b0eb7984407320e231
monia7dev <101042775+monia7dev@users.noreply.github.com>
2022-10-08 12:33:47 +0100

    add Models

--- simply-together/ActivitiesService.cs
+++ simply-together/ActivitiesService.cs
@@ -1,3 +1,34 @@
+using Newtonsoft.Json;
+using RestSharp;
+using simply_together;
+using simply_together.Models;
+
 public class ActivitiesService
 {
+    public void GetCategories()
+    {
+
+        var client = new RestClient("http://www.boredpi.com/api");
+        var request = new RestRequest("/api/activity/");
+        var response = client.ExecuteAsync(request);
+
+        if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            string rawResponse = response.Result.Content;
+            var serialize = JsonConvert.DeserializeObject<Categories>(rawResponse);
+
+            List<Category> returnedList = serialize.CategoriesList;
+
+            TableVisualisationEngine.ShowTable(returnedList, "Categories");
+
+        }
+
+
+
+    }
 }
+
+
+
+
+
