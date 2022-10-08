de37d1ffe0698e200673057ce498b79d574f0f85
monia7dev <101042775+monia7dev@users.noreply.github.com>
2022-10-08 11:30:27 +0100

    create TableVisualisationEngine method

--- simply-together/TableVisualisationEngine.cs
+++ simply-together/TableVisualisationEngine.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using ConsoleTableExt;
 
 namespace simply_together
 {
     public class TableVisualisationEngine
     {
-        
+        public static void ShowTable<T>(List<T> tableData, [AllowNull] string tableName) where T: class
+        {
+            Console.Clear();
+            if (tableName == null)
+                tableName = "";
+
+            Console.WriteLine("\n\n");
+
+            ConsoleTableBuilder
+                .From(tableData)
+                .WithColumn(tableName)
+                .ExportAndWriteLine();
+            Console.WriteLine("\n\n");
+        }
     }
 }
