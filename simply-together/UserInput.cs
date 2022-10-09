9100cac5713be58280bb9be2fcd200c81128faf1
monia7dev <101042775+monia7dev@users.noreply.github.com>
2022-10-09 10:56:54 +0100

    Delete and refactor in order to start again

--- simply-together/UserInput.cs
+++ simply-together/UserInput.cs
@@ -1,8 +1,29 @@
+using simply_together;
+
 public class UserInput
 {
         ActivitiesService activitiesService = new();
 
-        internal void GetCategoriesInput(){
+        internal void GetCategoriesInput()
+        {
             activitiesService.GetCategories();
+
+            Console.WriteLine("Choose category:");
+
+            string category = Console.ReadLine();
+
+            while (!Validator.IsStringValid(category))
+            {
+                Console.WriteLine("\nInvalid category");
+                category = Console.ReadLine();
+
+            }
+
+            GetDrinksInput(category);
         }
+
+    private void GetDrinksInput(string category)
+    {
+        activitiesService.GetDrinksByCategory(category);
+    }
 }
