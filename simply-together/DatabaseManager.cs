ba55232d7d2bec37a89f4b6c29b2fa849e3f9f6b
monia7dev <101042775+monia7dev@users.noreply.github.com>
2022-10-09 18:46:36 +0100

    create a table for the database

--- simply-together/DatabaseManager.cs
+++ simply-together/DatabaseManager.cs
@@ -1,9 +1,55 @@
 using System;
+using Microsoft.Data.Sqlite;
+
 
 namespace simply_together
 {
-    public class DatabaseManager
+    
+
+    internal class DatabaseManager
     {
-        
+        /// <summary>
+        /// Creates a database with a table if it doesn't exist.
+        /// </summary>
+        /// <param name="connectionString"></param>
+        internal void CreateTable(string connectionString)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+
+                    tableCmd.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS habits (
+                          Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                          Activity TEXT,
+                          Type TEXT,
+                          Participants TEXT
+                          Price TEXT
+                          Link TEXT
+                          Key TEXT
+                          Accessibility TEXT
+                        )";
+                    
+                    tableCmd.ExecuteNonQuery(); 
+                }
+
+
+
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
