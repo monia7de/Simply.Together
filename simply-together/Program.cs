60a32c5190718a8cf600c0d5bd06767c638e66bb
monia7dev <101042775+monia7dev@users.noreply.github.com>
2022-10-09 19:14:28 +0100

    add a call for the databaseManager

--- simply-together/Program.cs
+++ simply-together/Program.cs
@@ -1,12 +1,18 @@
-﻿
+﻿using System.Configuration;
+
 
 
 using simply_together;
 
 class Program
 {
+
+    static string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
     static void Main(string[] args)
     {
+        DatabaseManager databaseManager = new();
+        databaseManager.CreateTable(connectionString);
+
         UserInput userInput = new();
         userInput.GetDrinkTypesInput(); 
     }
